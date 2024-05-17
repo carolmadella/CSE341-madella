fea616a8c595b0f6392a4381a749f336b0dec973
Carolina Madella <carolina.madella@gmail.com>
2024-05-17 18:16:48 -0500

    changing the routes to be simpler

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 var express = require("express");
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 var app = express();
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "CSE341 API",
+      version: "1.0.0",
+    },
+    servers: [{ url: "http://localhost:5500" }],
+  },
+  apis: ["./routes/*.js"], // Use a glob pattern to include all route files
+};
+
+const swaggerSpecs = swaggerJsdoc(options);
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use("/", require("./routes")); // from previous lesson
-app.use("/contacts", require("./routes/contacts.js"));
+// Routes configuration
+app.use("/", require("./routes"));
+
+// Serve Swagger documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 app.listen(5500, () => {
   console.log("Server is running on Port 5500");
